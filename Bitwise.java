5b4fdab02809f3e05dde7d9282998563db822b47
iqbal8699 <rehandude2214@gmail.com>
2021-11-18 22:08:31 +0530

    this is my bitwise operating java program

---
+++ Bitwise.java
@@ -0,0 +1,52 @@
+class Bitwise 
+{
+public static void main(String[]args) // x-or operator
+{
+System.out.println(9^5); // 1st change decimal into binary, follow truth table then convert binary into decimal
+System.out.println(17^6);
+System.out.println(6^3);
+}
+}
+
+
+//for X-OR operator truth table is
+// 0 0 - 0
+// 0 1 - 1
+// 1 0 - 1
+// 1 1 - 0
+
+
+//for OR operator truth table is followed
+// 0 0 - 0
+// 0 1 - 1
+// 1 0 - 1
+// 1 1 - 1
+
+class Bitwise_2
+{
+public static void main(String[]args)
+{
+System.out.println(5|2);
+System.out.println(8|4);
+}
+}
+
+
+// truth table for AND operator
+// 0 0 - 0
+// 0 1 - 0
+// 1 0 - 0
+// 1 1 - 1
+
+class Bitwise_3
+{
+public static void main(String[]args)
+{
+System.out.println(9&5);
+System.out.println(5&8);
+System.out.println(true&false);
+System.out.println(3>=3&14==14);
+}
+}
+
+
